d8560ebe06174428b259940d6513b68cb9a741f5
Byoungju Park <pbyoungju@icloud.com>
2021-12-11 01:27:37 +0900

    initial commit

---
+++ src/test/java/com/pbyoungju/config/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pbyoungju.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
